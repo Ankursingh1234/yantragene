fc2adb715bef992623f11b4d063108f2c2377f71
Ankursingh1234 <ankursgh70@gmail.com>
2017-05-23 00:07:37 +0530

    Add files via upload

---
+++ App_Code/Databs.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Databs
+{
+    SqlCommand cmd;
+    SqlConnection con;
+    SqlDataAdapter Adapter;
+    string constr = "Data Source=localhost;Initial Catalog=yantragene17;User ID=sa;Password=ankur123";
+    public Databs()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+        try
+        {
+            con = new SqlConnection(constr);
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            else
+                con.Close();
+        }
+        catch (Exception e) { }
+    }
+
+    public DataTable verifys(string query)
+    {
+
+        Adapter = new SqlDataAdapter(query, con);
+        DataTable dt = new DataTable();
+        try
+        {
+            Adapter.Fill(dt);
+        }
+        catch { }
+
+        return dt;
+
+    }
+    public void InsertData(string query)
+    {
+        cmd = new SqlCommand(query, con);
+        cmd.ExecuteNonQuery();
+
+    }
+    public void UpdateData(string query)
+    {
+        cmd = new SqlCommand(query, con);
+        cmd.ExecuteNonQuery();
+
+    }
+
+    public void DeleteData(string query)
+    {
+        cmd = new SqlCommand(query, con);
+        cmd.ExecuteNonQuery();
+
+    }
+}
